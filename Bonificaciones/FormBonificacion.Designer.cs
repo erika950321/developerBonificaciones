b23fdd09fb5047630d91f3b5ca69c22480c968ec
carlos.paez <carlos.paez@certicamara.com>
2022-03-04 21:41:07 -0500

    Creación del formBonificacion para carga de CSV

--- Bonificaciones/FormBonificacion.Designer.cs
+++ Bonificaciones/FormBonificacion.Designer.cs
@@ -29,13 +29,62 @@ namespace Bonificaciones
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.dgvEmpleados = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmpleados)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(12, 12);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(75, 23);
+            this.btnCargar.TabIndex = 0;
+            this.btnCargar.Text = "Examinar";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // dgvEmpleados
+            // 
+            this.dgvEmpleados.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvEmpleados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEmpleados.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvEmpleados.Location = new System.Drawing.Point(3, 16);
+            this.dgvEmpleados.Name = "dgvEmpleados";
+            this.dgvEmpleados.Size = new System.Drawing.Size(770, 367);
+            this.dgvEmpleados.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dgvEmpleados);
+            this.groupBox1.Location = new System.Drawing.Point(12, 41);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 386);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // FormBonificacion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnCargar);
+            this.Name = "FormBonificacion";
+            this.Text = "FormBonificacion";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmpleados)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.DataGridView dgvEmpleados;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
--- Bonificaciones/FormBonificacion.cs
+++ Bonificaciones/FormBonificacion.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,40 @@ namespace Bonificaciones
         public FormBonificacion()
         {
             InitializeComponent();
+        }
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            var empleados = new List<string[]>();
+            OpenFileDialog ofd = new OpenFileDialog() { Filter = "Archivo CSV|*.csv" };
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string[] lineas = File.ReadAllLines(ofd.FileName);
+
+                string[] encabezado = lineas[0].Split(';');
+                dgvEmpleados.Columns.Clear();
+                foreach (string c in encabezado)
+                {
+                    dgvEmpleados.Columns.Add(c, c);
+                }
+
+                for (int i = 1; i < lineas.Length; i++)
+                {
+                    string[] celdas = lineas[i].Split(';');
+                    if (!celdas[0].Equals(""))
+                    {
+                        dgvEmpleados.Rows.Add(celdas);
+                    }
+                    empleados.Add(celdas);
+                }
+                ListarEmpleados(empleados);
+            }
+            //btnCargar.Enabled = false;
+        }
+        private List<string[]> ListarEmpleados(List<string[]> empleados)
+        {
+            var listaDepurada = new List<string[]>(empleados);
+            return listaDepurada;
         }
     }
 }
